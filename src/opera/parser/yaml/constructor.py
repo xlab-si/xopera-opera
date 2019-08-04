85a3f1b07c9f4a364d2aac7199cfc7e665b92bb0
Tadej Borovšak <tadej.borovsak@xlab.si>
2019-08-04 17:14:16 +0200

    Add YAML constructor
    Our constructor wraps the parsed values from the source document into
    YAML AST nodes with attached locations. The raw values are parsed
    according to the YAML 1.2 standard.

---
+++ src/opera/parser/yaml/constructor.py
@@ -0,0 +1,74 @@
+from yaml.constructor import BaseConstructor, ConstructorError
+
+from .node import Node
+
+
+class Constructor(BaseConstructor):
+    def __init__(self, stream_name):
+        super().__init__()
+        self._stream_name = stream_name
+
+    def _pos(self, node):
+        # Convert 0-based indices to 1-based (text editor) marks
+        return dict(
+            stream_name=self._stream_name,
+            line=node.start_mark.line + 1,
+            column=node.start_mark.column + 1,
+        )
+
+    def construct_yaml_null(self, node):
+        self.construct_scalar(node)
+        return Node(None, **self._pos(node))
+
+    def construct_yaml_bool(self, node):
+        value = self.construct_scalar(node).lower()
+        return Node(value == "true", **self._pos(node))
+
+    def construct_yaml_int(self, node):
+        value = self.construct_scalar(node)
+        if value.startswith("0o"):
+            base = 8
+        elif value.startswith("0x"):
+            base = 16
+        else:
+            base = 10
+        return Node(int(value, base=base), **self._pos(node))
+
+    def construct_yaml_float(self, node):
+        value = self.construct_scalar(node).lower()
+        if value == ".nan":
+            value = "nan"
+        elif value.endswith(".inf"):
+            value = value.replace(".", "")
+        return Node(float(value), **self._pos(node))
+
+    def construct_yaml_str(self, node):
+        return Node(self.construct_scalar(node), **self._pos(node))
+
+    def construct_yaml_seq(self, node):
+        data = Node([], **self._pos(node))
+        yield data
+        data.value.extend(self.construct_sequence(node))
+
+    def construct_yaml_map(self, node):
+        data = Node({}, **self._pos(node))
+        yield data
+        data.value.update(self.construct_mapping(node))
+
+    def construct_undefined(self, node):
+        raise ConstructorError(
+            None, None,
+            "could not determine a constructor for {}".format(node.tag),
+            node.start_mark,
+        )
+
+
+# Add constructors for YAML 1.2 types (core schema).
+for tag in ("null", "bool", "int", "float", "str", "seq", "map"):
+    Constructor.add_constructor(
+        "tag:yaml.org,2002:{}".format(tag),
+        getattr(Constructor, "construct_yaml_{}".format(tag)),
+    )
+
+# Error reporter.
+Constructor.add_constructor(None, Constructor.construct_undefined)
