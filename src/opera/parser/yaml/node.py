32ea3c04ed7d6bcaa1c539fdaa78e6629bdcb308
Tadej Borovšak <tadej.borovsak@xlab.si>
2019-08-04 15:08:46 +0200

    Add YAML AST node
    This node is a location-aware wrapper around the native type of data
    (type(None), bool, int, float, str, list and dist). The location
    information should be retained throughout the data lifecycle in order
    for us to be able to produce sensible error messages.

---
+++ src/opera/parser/yaml/node.py
@@ -0,0 +1,44 @@
+from opera.parser.utils.location import Location
+
+
+class Node:
+    def __init__(self, value, stream_name, line, column):
+        self.value = value
+        self.loc = Location(stream_name, line, column)
+
+    def _dump_header(self):
+        return "{}({})".format(type(self.value).__name__, self.loc)
+
+    def _dump_map(self, pad):
+        children = (
+            "  {}{}: {}".format(pad, k.dump(pad + "  "), v.dump(pad + "  "))
+            for k, v in self.value.items()
+        )
+        return "{head}{{\n{children}\n{pad}}}".format(
+            head=self._dump_header(), children=",\n".join(children), pad=pad,
+        )
+
+    def _dump_seq(self, pad):
+        children = (pad + "  " + v.dump(pad + "  ") for v in self.value)
+        return "{head}[\n{children}\n{pad}]".format(
+            head=self._dump_header(), children=",\n".join(children), pad=pad,
+        )
+
+    def dump(self, pad=""):
+        if type(self.value) is list:
+            return self._dump_seq(pad)
+        if type(self.value) is dict:
+            return self._dump_map(pad)
+        # No padding for scalars, since the are inlined in the dump.
+        return "{}({})".format(self._dump_header(), self.value)
+
+    @property
+    def bare(self):
+        if type(self.value) is list:
+            return [v.bare for v in self.value]
+        if type(self.value) is dict:
+            return {k.bare: v.bare for k, v in self.value.items()}
+        return self.value
+
+    def __str__(self):
+        return self.dump()
