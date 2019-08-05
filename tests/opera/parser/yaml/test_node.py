488e57fcec78268beabdc1c0ef2b328322c41d82
Tadej Borovšak <tadej.borovsak@xlab.si>
2019-08-05 18:10:19 +0200

    Completely decouple YAML node and location marker
    Up until now, the separation was only superficial, since we provided
    all the parameter for location construction via node constructor
    parameter.
    
    In this commit, we separated out the location parameters and modified
    node constructor that now takes location object as a parameter.
    
    With this clean separation, we also gained a concise way of
    construction YAML trees, since location parameter can be made optional
    with little to no work.

--- tests/opera/parser/yaml/test_node.py
+++ tests/opera/parser/yaml/test_node.py
@@ -1,59 +1,71 @@
+from opera.parser.utils.location import Location
 from opera.parser.yaml.node import Node
 
 
 class TestInit:
     def test_constructor(self):
-        node = Node("value", "name", 1, 2)
+        node = Node("value", Location("name", 1, 2))
 
         assert node.value == "value"
         assert node.loc.stream_name == "name"
         assert node.loc.line == 1
         assert node.loc.column == 2
 
+    def test_empty_loc_constructor(self):
+        node = Node("value")
+
+        assert node.value == "value"
+        assert node.loc.stream_name == ""
+        assert node.loc.line == 0
+        assert node.loc.column == 0
+
 
 class TestDump:
     def test_null_dump(self):
-        node = Node(None, "s", 1, 1)
+        node = Node(None, Location("s", 1, 1))
 
         assert node.dump() == "NoneType(s:1:1)(None)"
 
     def test_bool_dump(self):
-        node = Node(True, "s", 1, 1)
+        node = Node(True, Location("s", 1, 1))
 
         assert node.dump() == "bool(s:1:1)(True)"
 
     def test_int_dump(self):
-        node = Node(123, "s", 1, 1)
+        node = Node(123, Location("s", 1, 1))
 
         assert node.dump() == "int(s:1:1)(123)"
 
     def test_sfloat_dump(self):
-        node = Node(123.456, "s", 1, 1)
+        node = Node(123.456, Location("s", 1, 1))
 
         assert node.dump() == "float(s:1:1)(123.456)"
 
     def test_str_dump(self):
-        node = Node("string", "s", 1, 1)
+        node = Node("string", Location("s", 1, 1))
 
         assert node.dump() == "str(s:1:1)(string)"
 
     def test_scalar_ignores_padding(self):
-        node = Node("string", "s", 1, 1)
+        node = Node("string", Location("s", 1, 1))
 
         assert node.dump("    ") == "str(s:1:1)(string)"
 
     def test_seq_dump(self):
-        node = Node([Node(1, "s", 1, 1), Node("a", "k", 2, 3)], "u", 5, 6)
+        node = Node([Node(1), Node("a")])
 
         assert node.dump() == (
-            "list(u:5:6)[\n"
-            "  int(s:1:1)(1),\n"
-            "  str(k:2:3)(a)\n"
+            "list(:0:0)[\n"
+            "  int(:0:0)(1),\n"
+            "  str(:0:0)(a)\n"
             "]"
         )
 
     def test_seq_dump_padded(self):
-        node = Node([Node(1, "s", 1, 1), Node("a", "k", 2, 3)], "u", 5, 6)
+        node = Node([
+            Node(1, Location("s", 1, 1)),
+            Node("a", Location("k", 2, 3)),
+        ], Location("u", 5, 6))
 
         assert node.dump("   ") == (
             "list(u:5:6)[\n"
@@ -63,32 +75,33 @@ class TestDump:
         )
 
     def test_map_dump(self):
-        node = Node({Node(1, "s", 1, 1): Node("a", "k", 2, 3)}, "u", 5, 6)
+        node = Node({Node(1): Node("a")})
 
         assert node.dump() == (
-            "dict(u:5:6){\n"
-            "  int(s:1:1)(1): str(k:2:3)(a)\n"
+            "dict(:0:0){\n"
+            "  int(:0:0)(1): str(:0:0)(a)\n"
             "}"
         )
 
     def test_map_dump_padded(self):
-        node = Node({Node(1, "s", 1, 1): Node("a", "k", 2, 3)}, "u", 5, 6)
+        node = Node({Node(1): Node("a")})
 
         assert node.dump("      ") == (
-            "dict(u:5:6){\n"
-            "        int(s:1:1)(1): str(k:2:3)(a)\n"
+            "dict(:0:0){\n"
+            "        int(:0:0)(1): str(:0:0)(a)\n"
             "      }"
         )
 
     def test_dump_nested(self):
-        child = Node([Node(1, "a", 1, 2), Node("a", "b", 3, 4)], "c", 5, 6)
-        node = Node({Node(True, "d", 7, 8): child}, "e", 9, 0)
+        node = Node({
+            Node(True): Node([Node(1), Node("a")])
+        })
 
         assert node.dump() == (
-            "dict(e:9:0){\n"
-            "  bool(d:7:8)(True): list(c:5:6)[\n"
-            "    int(a:1:2)(1),\n"
-            "    str(b:3:4)(a)\n"
+            "dict(:0:0){\n"
+            "  bool(:0:0)(True): list(:0:0)[\n"
+            "    int(:0:0)(1),\n"
+            "    str(:0:0)(a)\n"
             "  ]\n"
             "}"
         )
@@ -96,25 +109,22 @@ class TestDump:
 
 class TestBare:
     def test_bare_scalar(self):
-        assert Node(1, "a", 2, 3).bare == 1
+        assert Node(1).bare == 1
 
     def test_bare_seq(self):
-        node = Node([Node(1, "s", 1, 1), Node("a", "k", 2, 3)], "u", 5, 6)
-
-        assert node.bare == [1, "a"]
+        assert Node([Node(1), Node("a")]).bare == [1, "a"]
 
     def test_bare_map(self):
-        node = Node({Node(1, "s", 1, 1): Node("a", "k", 2, 3)}, "u", 5, 6)
-
-        assert node.bare == {1: "a"}
+        assert Node({Node(1): Node("a")}).bare == {1: "a"}
 
     def test_base_nested(self):
-        child = Node([Node(1, "s", 1, 1), Node("a", "k", 2, 3)], "u", 5, 6)
-        node = Node({Node(1, "s", 1, 1): child}, "u", 5, 6)
+        node = Node({
+            Node(1): Node([Node(1), Node("a")]),
+        })
 
         assert node.bare == {1: [1, "a"]}
 
 
 class TestStr:
     def test_str(self):
-        assert str(Node(0, "a", 1, 2)) == "int(a:1:2)(0)"
+        assert str(Node(0, Location("a", 1, 2))) == "int(a:1:2)(0)"
