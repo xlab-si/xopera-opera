d6b7fac17d4c1320656e27a928de64a5ccc3bb0d
Tadej Borovšak <tadej.borovsak@xlab.si>
2019-08-09 20:57:14 +0200

    Add yaml parser test fixture

---
+++ tests/opera/parser/tosca/conftest.py
@@ -0,0 +1,12 @@
+import textwrap
+
+import pytest
+
+from opera.parser import yaml
+
+
+@pytest.fixture
+def yaml_ast():
+    def _yaml_ast(string_data):
+        return yaml.load(textwrap.dedent(string_data), "TEST")
+    return _yaml_ast
