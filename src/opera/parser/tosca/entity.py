daa60de6e7488232b4fe36d8c5f93d0305eda0e5
Tadej Borovšak <tadej.borovsak@xlab.si>
2019-08-09 19:23:47 +0200

    Add entity and type entity parsers
    Entity is a class that should be used as a base class for all structured
    TOSCA parser classes. Entity class provides built-in validator for
    allowed/required fields and checks that ensure dictionary keys are
    all strings.
    
    Another piece of functionality that this class provides is the automatic
    recursive parsing. All declared attributes are automatically parsed by
    the designated parser.
    
    Type entity class should be used to create new type parsers since this
    class has definitions for common fields and requirements of type
    definitions.

---
+++ src/opera/parser/tosca/entity.py
@@ -0,0 +1,106 @@
+from .base import Base
+from .map import Map
+from .reference import Reference
+from .string import String
+from .version import Version
+
+
+class Entity(Base):
+    ATTRS = {}  # This must be overridden in derived classes
+    REQUIRED = set()  # This can be overriden in derived classes
+
+    @classmethod
+    def validate(cls, yaml_node):
+        assert cls.ATTRS != {}
+        assert isinstance(cls.REQUIRED, set)
+
+        if not isinstance(yaml_node.value, dict):
+            cls.abort("Expected map.", yaml_node.loc)
+
+        data_keys = set()
+        for k in yaml_node.value:
+            if not isinstance(k.value, str):
+                cls.abort("Expected string", k.loc)
+            data_keys.add(k.value)
+
+        missing_keys = cls.REQUIRED - data_keys
+        if missing_keys:
+            cls.abort(
+                "Missing required fields: {}".format(", ".join(missing_keys)),
+                yaml_node.loc,
+            )
+
+        extra_keys = data_keys - cls.attrs().keys()
+        if extra_keys:
+            cls.abort(
+                "Invalid keys: {}".format(", ".join(extra_keys)),
+                yaml_node.loc,
+            )
+
+    @classmethod
+    def build(cls, yaml_node):
+        classes = cls.attrs()
+        data = {
+            k.value: classes[k.value].parse(v)
+            for k, v in yaml_node.value.items()
+        }
+        return cls(data, yaml_node.loc)
+
+    @classmethod
+    def attrs(cls):
+        return cls.ATTRS
+
+    def __getattr__(self, key):
+        try:
+            return self.data[key]
+        except KeyError:
+            raise AttributeError(key)
+
+    def __getitem__(self, key):
+        return self.data[key]
+
+    def __iter__(self):
+        return iter(self.data)
+
+    def dig(self, key, *subpath):
+        if key not in self.data:
+            return None
+        if not subpath:
+            return self.data[key]
+        return self.data[key].dig(*subpath)
+
+    def items(self):
+        return self.data.items()
+
+    def values(self):
+        return self.data.values()
+
+    @property
+    def bare(self):
+        return {k: v.bare for k, v in self.data.items()}
+
+
+class TypeEntity(Entity):
+    REFERENCE = None  # Override in subclasses
+
+    @classmethod
+    def validate(cls, yaml_node):
+        super().validate(yaml_node)
+        for key in yaml_node.value:
+            if key.value == "derived_from":
+                return
+        cls.abort("Type is missing derived_from key.", yaml_node.loc)
+
+    @classmethod
+    def attrs(cls):
+        assert isinstance(cls.REFERENCE, Reference), \
+            "Override REFERENCE in {} with Reference.".format(cls.__name__)
+
+        attributes = cls.ATTRS.copy()
+        attributes.update(
+            derived_from=cls.REFERENCE,
+            description=String,
+            metadata=Map(String),
+            version=Version,
+        )
+        return attributes
