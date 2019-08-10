29e5e0374d0ad03cbec09bd0992d083a9fe78d01
Tadej Borovšak <tadej.borovsak@xlab.si>
2019-08-10 15:38:46 +0200

    Add relationship type parser

---
+++ src/opera/parser/tosca/v_1_3/relationship_type.py
@@ -0,0 +1,19 @@
+from ..entity import TypeEntity
+from ..list import List
+from ..map import Map
+from ..reference import Reference
+from ..string import String
+
+from .attribute_definition import AttributeDefinition
+from .interface_definition_for_type import InterfaceDefinitionForType
+from .property_definition import PropertyDefinition
+
+
+class RelationshipType(TypeEntity):
+    REFERENCE = Reference("relationship_types")
+    ATTRS = dict(
+        properties=Map(PropertyDefinition),
+        attributes=Map(AttributeDefinition),
+        interfaces=Map(InterfaceDefinitionForType),
+        valid_target_types=List(Reference("capability_types")),
+    )
