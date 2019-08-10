5f8a5a12d2a735ef9dc676d487f9f66672a5d3be
Tadej Borovšak <tadej.borovsak@xlab.si>
2019-08-10 13:21:44 +0200

    Add data type parser

---
+++ src/opera/parser/tosca/v_1_3/data_type.py
@@ -0,0 +1,34 @@
+from opera.parser.yaml.node import Node
+
+from ..entity import TypeEntity
+from ..list import List
+from ..map import Map
+from ..reference import Reference
+from ..string import String
+
+from .constraint_clause import ConstraintClause
+from .property_definition import PropertyDefinition
+
+
+class DataType(TypeEntity):
+    REFERENCE = Reference("data_types")
+    ATTRS = dict(
+        constraints=List(ConstraintClause),
+        properties=Map(PropertyDefinition),
+    )
+
+    @classmethod
+    def normalize(cls, yaml_node):
+        # Let the validator handle non-dict case
+        if not isinstance(yaml_node.value, dict):
+            return yaml_node
+
+        # Make sure we have derived_from key
+        for k in yaml_node.value:
+            if k.value == "derived_from":
+                return yaml_node
+
+        # Create default derived_from spec if missing
+        data = {Node("derived_from"): Node("None")}
+        data.update(yaml_node.value)
+        return Node(data, yaml_node.loc)
