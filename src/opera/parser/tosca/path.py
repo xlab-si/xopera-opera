9f334d044e2a3f371e1dc9cf500a205d0cb56f6a
Tadej Borovšak <tadej.borovsak@xlab.si>
2019-08-12 20:22:51 +0200

    Add path canonicalization

--- src/opera/parser/tosca/path.py
+++ src/opera/parser/tosca/path.py
@@ -7,3 +7,32 @@ class Path(String):
     @classmethod
     def build(cls, yaml_node):
         return cls(pathlib.PurePath(yaml_node.value), yaml_node.loc)
+
+    def canonicalize_paths(self, parent_path):
+        if self.data.is_absolute():
+            return
+
+        # Next loop removes as many path/.. pairs as possible. When the path
+        # is in its canonical form, it should not start with .. since that
+        # would mean that something is trying to access paths outside the
+        # CSAR. But during the import process, .. can be present at the
+        # beggining of a path since we are processing the data in the
+        # depth-first order and later canonicalizations might eliminate them.
+        #
+        # Examples:
+        #     some/path/..  -> some
+        #     ../paths/here -> ../paths/here
+        #     my/../../path -> ../path
+
+        pos = 1
+        parts = list(parent_path.parts) + list(self.data.parts)
+
+        while pos < len(parts):
+            if parts[pos] == ".." and parts[pos - 1] != "..":
+                del parts[pos]
+                del parts[pos - 1]
+                pos = max(pos - 1, 1)
+            else:
+                pos += 1
+
+        self.data = pathlib.PurePath(*parts)
--- tests/opera/parser/tosca/test_path.py
+++ tests/opera/parser/tosca/test_path.py
@@ -10,3 +10,21 @@ from opera.parser.yaml.node import Node
 class TestBuild:
     def test_build(self):
         assert isinstance(Path.build(Node("/")).data, pathlib.PurePath)
+
+
+class TestCanonicalizePaths:
+    @pytest.mark.parametrize("input,parent,output", [
+        ("/absolute/path", "base/path", "/absolute/path"),
+        ("/absolute/path", ".", "/absolute/path"),
+        ("rel/path", "base/path", "base/path/rel/path"),
+        ("rel/path", ".", "rel/path"),
+        ("rel/path", "..", "../rel/path"),
+        ("rel/path", "../..", "../../rel/path"),
+        ("../rel/path", "component", "rel/path"),
+        ("../../rel/path", "component", "../rel/path"),
+    ])
+    def test_canonicalization(self, input, parent, output):
+        path = Path(pathlib.PurePath(input), None)
+        path.canonicalize_paths(pathlib.PurePath(parent))
+
+        assert str(path.data) == output
