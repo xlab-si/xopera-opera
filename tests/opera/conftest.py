8e702bdf1c143393dcbef8a681032324a3d869aa
Tadej Borovšak <tadej.borovsak@xlab.si>
2019-08-13 17:11:45 +0200

    Add textual yaml content fixture

--- tests/opera/conftest.py
+++ tests/opera/conftest.py
@@ -6,6 +6,13 @@ from opera.parser import yaml
 
 
 @pytest.fixture
+def yaml_text():
+    def _yaml_text(string_data):
+        return textwrap.dedent(string_data)
+    return _yaml_text
+
+
+@pytest.fixture
 def yaml_ast():
     def _yaml_ast(string_data):
         return yaml.load(textwrap.dedent(string_data), "TEST")
