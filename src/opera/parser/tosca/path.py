eb143ac72bff674055d77aa15b912527c6361e21
Sašo Stanovnik <saso.stanovnik@xlab.si>
2019-10-09 14:18:56 +0200

    Prevent absolute paths from being parsed in path nodes.

--- src/opera/parser/tosca/path.py
+++ src/opera/parser/tosca/path.py
@@ -1,5 +1,6 @@
 import pathlib
 
+from opera.error import ParseError
 from opera.parser.yaml.node import Node
 from .string import String
 
@@ -7,7 +8,11 @@ from .string import String
 class ToscaPath(String):
     @classmethod
     def build(cls, yaml_node: Node):
-        return cls(pathlib.PurePath(yaml_node.value), yaml_node.loc)
+        path = pathlib.PurePath(yaml_node.value)
+        # a deviation from the TOSCA standard
+        if path.is_absolute():
+            raise ParseError("Absolute paths are not supported in xOpera.", yaml_node.loc)
+        return cls(path, yaml_node.loc)
 
     @property
     def bare(self):
--- tests/opera/parser/tosca/test_path.py
+++ tests/opera/parser/tosca/test_path.py
@@ -1,9 +1,18 @@
 import pathlib
 
+import pytest
+
+from opera.error import ParseError
 from opera.parser.tosca.path import ToscaPath
 from opera.parser.yaml.node import Node
 
 
 class TestBuild:
-    def test_build(self):
-        assert isinstance(ToscaPath.build(Node("/")).data, pathlib.PurePath)
+    @pytest.mark.parametrize("path", ["a.file", "../a.file", "b/../a.file"])
+    def test_build_relative(self, path):
+        assert isinstance(ToscaPath.build(Node(path)).data, pathlib.PurePath)
+
+    @pytest.mark.parametrize("path", ["/etc", "/etc/hosts", "/dev/zero", "/doesnotexist", "/does/not/exist"])
+    def test_build_absolute(self, path):
+        with pytest.raises(ParseError):
+            ToscaPath.build(Node(path))
