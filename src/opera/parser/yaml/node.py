6f1c16309565cc85315778b7853130cf4b9d973c
Tadej Borovšak <tadej.borovsak@xlab.si>
2020-01-27 17:37:52 +0100

    Simplify YAML AST node

--- src/opera/parser/yaml/node.py
+++ src/opera/parser/yaml/node.py
@@ -6,32 +6,6 @@ class Node:
         self.value = value
         self.loc = loc or Location("", 0, 0)
 
-    def _dump_header(self):
-        return "{}({})".format(type(self.value).__name__, self.loc)
-
-    def _dump_map(self, pad):
-        children = (
-            "  {}{}: {}".format(pad, k.dump(pad + "  "), v.dump(pad + "  "))
-            for k, v in self.value.items()
-        )
-        return "{head}{{\n{children}\n{pad}}}".format(
-            head=self._dump_header(), children=",\n".join(children), pad=pad,
-        )
-
-    def _dump_seq(self, pad):
-        children = (pad + "  " + v.dump(pad + "  ") for v in self.value)
-        return "{head}[\n{children}\n{pad}]".format(
-            head=self._dump_header(), children=",\n".join(children), pad=pad,
-        )
-
-    def dump(self, pad=""):
-        if type(self.value) is list:
-            return self._dump_seq(pad)
-        if type(self.value) is dict:
-            return self._dump_map(pad)
-        # No padding for scalars, since the are inlined in the dump.
-        return "{}({})".format(self._dump_header(), self.value)
-
     @property
     def bare(self):
         if type(self.value) is list:
@@ -41,4 +15,4 @@ class Node:
         return self.value
 
     def __str__(self):
-        return self.dump()
+        return "Node({})[{}]".format(self.loc, self.bare)
