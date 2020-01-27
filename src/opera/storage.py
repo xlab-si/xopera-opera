94669aff210a9fc4257a9a91ea3a641a40954fff
Tadej Borovšak <tadej.borovsak@xlab.si>
2020-01-27 18:14:44 +0100

    Add storage interface

---
+++ src/opera/storage.py
@@ -0,0 +1,24 @@
+import json
+import pathlib
+
+
+class Storage:
+    def __init__(self, path):
+        self.path = path
+
+        path.mkdir(exist_ok=True)
+
+    def write(self, content, *path):
+        *subpath, name = path
+        dir_path = self.path / pathlib.PurePath(*subpath)
+        dir_path.mkdir(exist_ok=True, parents=True)
+        (dir_path / name).write_text(content)
+
+    def read(self, *path):
+        return (self.path / pathlib.PurePath(*path)).read_text()
+
+    def write_json(self, data, *path):
+        self.write(json.dumps(data, indent=2), *path)
+
+    def read_json(self, *path):
+        return json.loads(self.read(*path))
