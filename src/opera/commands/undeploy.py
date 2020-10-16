c5c9599f076597b555cddd4e273ac144eff01c45
anzoman <aluzarwork@gmail.com>
2020-10-16 10:30:32 +0200

    Add missing CLI command flags for opera undeploy
    Let's introduce --resume/-r and --force/-f opera undeploy command
    switches that are similar to opera deploy command flags. When -r
    option is used opera will resume the undeployment process from where
    it was interrupted (i.e. was stopped or has failed). Another option is
    -f which tells opera to skip any possible prompts.
    
    Similar than in the previous commit we used opera info state with the
    newly applied CLI flags to achieve cleaner performance. As we were
    doing all that, the new and old python imports needed to be fixed too.

--- src/opera/commands/undeploy.py
+++ src/opera/commands/undeploy.py
@@ -1,10 +1,12 @@
 import argparse
 from pathlib import Path, PurePath
+from os import path
 
+from opera.commands.info import info
+from opera.utils import prompt_yes_no_question
 from opera.error import DataError, ParseError
 from opera.parser import tosca
 from opera.storage import Storage
-from os import path, chdir
 
 
 def add_parser(subparsers):
@@ -23,6 +25,14 @@ def add_parser(subparsers):
         type=int, default=1
     )
     parser.add_argument(
+        "--resume", "-r", action='store_true',
+        help="Resume the undeployment from where it was interrupted",
+    )
+    parser.add_argument(
+        "--force", "-f", action='store_true',
+        help="Force the action and skip any possible prompts",
+    )
+    parser.add_argument(
         "--verbose", "-v", action='store_true',
         help="Turns on verbose mode",
     )
@@ -31,14 +41,32 @@ def add_parser(subparsers):
 
 def _parser_callback(args):
     if args.instance_path and not path.isdir(args.instance_path):
-        raise argparse.ArgumentTypeError("Directory {0} is not a valid path!"
+        raise argparse.ArgumentTypeError("Directory {} is not a valid path!"
                                          .format(args.instance_path))
 
     if args.workers < 1:
-        print("{0} is not a positive number!".format(args.workers))
+        print("{} is not a positive number!".format(args.workers))
         return 1
 
     storage = Storage.create(args.instance_path)
+    status = info(storage)["status"]
+
+    if storage.exists("instances"):
+        if args.resume and status == "interrupted":
+            if not args.force:
+                print("The resume undeploy option might have unexpected "
+                      "consequences on the already undeployed blueprint.")
+                question = prompt_yes_no_question()
+                if not question:
+                    return 0
+        elif status == "interrupted":
+            print("The instance model already exists. Use --resume/-r option "
+                  "to continue current undeployment process.")
+            return 0
+        elif status == "undeployed":
+            print("All instances have already been undeployed.")
+            return 0
+
     try:
         undeploy(storage, args.verbose, args.workers)
     except ParseError as e:
@@ -56,18 +84,26 @@ def undeploy(storage: Storage, verbose_mode: bool, num_workers: int):
     :raises ParseError:
     :raises DataError:
     """
-    service_template = storage.read("root_file")
-    inputs = storage.read_json("inputs")
-
-    workdir = str(Path.cwd())
-    if storage.exists("csars"):
-        csar_dir = Path(storage.path) / "csars" / "csar"
-        workdir = str(csar_dir)
-        ast = tosca.load(Path(csar_dir),
-                         PurePath(service_template).relative_to(csar_dir))
+    if storage.exists("inputs"):
+        inputs = storage.read_json("inputs")
     else:
-        ast = tosca.load(Path.cwd(), PurePath(service_template))
+        inputs = {}
 
-    template = ast.get_template(inputs)
-    topology = template.instantiate(storage)
-    topology.undeploy(verbose_mode, workdir, num_workers)
+    if storage.exists("root_file"):
+        service_template = storage.read("root_file")
+        workdir = str(Path.cwd())
+
+        if storage.exists("csars"):
+            csar_dir = Path(storage.path) / "csars" / "csar"
+            workdir = str(csar_dir)
+            ast = tosca.load(Path(csar_dir),
+                             PurePath(service_template).relative_to(csar_dir))
+        else:
+            ast = tosca.load(Path.cwd(), PurePath(service_template))
+
+        template = ast.get_template(inputs)
+        topology = template.instantiate(storage)
+        topology.undeploy(verbose_mode, workdir, num_workers)
+    else:
+        print("There is no root_file in storage.")
+        return 0
