f5a8dab67557168271acc0cbd9973dbfe102ae0b
anzoman <aluzarwork@gmail.com>
2020-10-16 09:45:13 +0200

    Rename misc tosca types integration test folder
    Here we will use underscores instead of hyphens to be  more aesthetic
    and to stick with the consistent file naming in opera where mostly
    underscores are used.

--- tests/integration/misc_tosca_types/runme.sh
+++ tests/integration/misc_tosca_types/runme.sh
@@ -6,38 +6,17 @@ opera_executable="$1"
 
 # perform integration test with TOSCA service template
 # test opera info contents before everything
-info_out="$($opera_executable info --format json)"
-test "$(echo "$info_out" | jq -r .status)" = "null"
-
-# validate
+$opera_executable info --format json
 $opera_executable validate -i inputs.yaml service-template.yaml
-# test opera info status after validate
-info_out="$($opera_executable info --format json)"
-test "$(echo "$info_out" | jq -r .status)" = "null"
-
-# init
+$opera_executable info --format yaml
 $opera_executable init service-template.yaml
-# test opera info status after init
-info_out="$($opera_executable info --format json)"
-test "$(echo "$info_out" | jq -r .status)" = "initialized"
-
-# deploy
+$opera_executable info -f json
 $opera_executable deploy -i inputs.yaml
-# test opera info status after deploy
-info_out="$($opera_executable info --format json)"
-test "$(echo "$info_out" | jq -r .status)" = "deployed"
-
-# outputs
+$opera_executable info -f yaml
 $opera_executable outputs
-# test opera info status after outputs
-info_out="$($opera_executable info --format json)"
-test "$(echo "$info_out" | jq -r .status)" = "deployed"
-
-# undeploy
+$opera_executable info -f json
 $opera_executable undeploy
-# test opera info status after undeploy
-info_out="$($opera_executable info --format json)"
-test "$(echo "$info_out" | jq -r .status)" = "undeployed"
+$opera_executable info
 
 # integration test with compressed CSAR
 rm -rf .opera
@@ -46,7 +25,7 @@ zip -r test.csar service-template.yaml modules TOSCA-Metadata
 mv test.csar csar-test
 mv inputs.yaml csar-test
 cd csar-test
-$opera_executable info --format yaml
+$opera_executable info -f yaml
 $opera_executable init -i inputs.yaml test.csar
 $opera_executable info -f json
 $opera_executable deploy
