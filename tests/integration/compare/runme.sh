3ee518eff1714a22f180f44e50897a43655d9fb6
anzoman <aluzarwork@gmail.com>
2021-05-14 10:07:36 +0200

    Speed up integration tests
    With these changes we will reshape our integration tests for their
    faster execution. We have erased some uneeded checks in the shell
    scripts and have separated integration test that tests execution
    interruption from the cli_commands, which will make the latter much
    faster.

--- tests/integration/compare/runme.sh
+++ tests/integration/compare/runme.sh
@@ -5,23 +5,23 @@ set -euo pipefail
 opera_executable="$1"
 
 # perform integration test
-$opera_executable deploy -i inputs1.yaml template1.yaml
+$opera_executable deploy -i inputs1.yaml service1.yaml
 
 # test opera info status after deploy
 info_out="$($opera_executable info --format json)"
 test "$(echo "$info_out" | jq -r .status)" = "deployed"
 
 # test opera diff output
-info_out="$($opera_executable diff -i inputs2.yaml template2.yaml --format json)"
+info_out="$($opera_executable diff -i inputs2.yaml service2.yaml --format json)"
 test "$(echo "$info_out" | jq -r .nodes.added[0])" = "hello-5"
 test "$(echo "$info_out" | jq -r .nodes.deleted[0])" = "hello-4"
 
-$opera_executable update -i inputs2.yaml template2.yaml
+$opera_executable update -i inputs2.yaml service2.yaml
 
 info_out="$($opera_executable info --format json)"
 test "$(echo "$info_out" | jq -r .status)" = "deployed"
 
-$opera_executable update -i inputs2.yaml template2.yaml
+$opera_executable update -i inputs2.yaml service2.yaml
 
 info_out="$($opera_executable info --format json)"
 test "$(echo "$info_out" | jq -r .status)" = "deployed"
