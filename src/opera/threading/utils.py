b1eb8c2a461da9ae4abe3b3d87ffb5abec59dba7
Alexander Maslennikov <amaslenn@gmail.com>
2022-01-03 16:59:55 +0100

    Fix for concurrent output issue
    The issue #234 describes a situation, when we have synchronous
    failure of multiple node operations, executed in multiple threads.
    A lock object was introduced to eliminate this issue.

--- src/opera/threading/utils.py
+++ src/opera/threading/utils.py
@@ -1,5 +1,17 @@
-from threading import current_thread  # type: ignore # pylint: disable=no-name-in-module
+from io import TextIOWrapper
+from threading import current_thread, Lock  # type: ignore # pylint: disable=no-name-in-module
 
 
 def print_thread(string):
     print(f"[{current_thread().name}] {string}")
+
+
+class SafePrinter:
+    lock = Lock()
+
+    @classmethod
+    def print_lines(cls, wrapper: TextIOWrapper):
+        with cls.lock:
+            print_thread("------------")
+            for line in wrapper:
+                print(line.rstrip())
