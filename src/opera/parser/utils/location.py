941835c59edd11dc34a80a14d5797f2679be3dc0
Alexander Maslennikov <amaslenn@gmail.com>
2022-04-14 10:42:26 +0200

    Remove entities now defined in TOSCA parser

--- src/opera/parser/utils/location.py
+++
@@ -1,8 +0,0 @@
-class Location:
-    def __init__(self, stream_name, line, column):
-        self.stream_name = stream_name
-        self.line = line
-        self.column = column
-
-    def __str__(self):
-        return f"{self.stream_name}:{self.line}:{self.column}"
--- src/opera/parser/yaml/__init__.py
+++
@@ -1,9 +0,0 @@
-from .loader import Loader
-
-
-def load(stream, stream_path):
-    ldr = Loader(stream, stream_path)
-    try:
-        return ldr.get_single_data()
-    finally:
-        ldr.dispose()
